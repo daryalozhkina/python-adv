bc7cdf56831b2b75494ddc32483961b3dbc3178b
ь <das.lozh19@mail.ru>
2021-01-31 18:54:55 +0500

    first step

--- 210125/step_2.py
+++ 210125/step_2.py
@@ -1,12 +1,23 @@
+
+
 alphabet = {chr(el) for el in range(ord('а'), ord('я') + 1)}
-alphabet.add('ё')
+
+alphabet.update(['ё', '-'])
+
+
 
 
 def name_is_valid(name):
     if not name or set(name.lower()) - alphabet:
         return False
+    if '-' in name:
+        for _name in name.split('-'):
+            if not _name.istitle():
+                return False
+            return True
     return name.istitle()
 
 
 print(name_is_valid('иван'))
 print(name_is_valid('Иван'))
+print(name_is_valid('Анна-Виктория'))
