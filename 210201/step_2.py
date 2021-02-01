4252ecd7d0e606ace458253648868c86681dbb0b
ь <das.lozh19@mail.ru>
2021-02-01 11:44:44 +0500

    request

--- 210201/step_2.py
+++ 210201/step_2.py
@@ -1,8 +1,16 @@
+import re
+
 import requests
+
+# RE_XLS_FILE = re.compile(r'href=".+"')
+RE_XLS_FILE = re.compile(r'href="[^"]+"')
 
 request_url = 'https://kpk.kss45.ru/учебная-работа/расписание_пар.html'
 
 response = requests.get(request_url)
 content = response.content.decode(encoding=response.encoding)
 
-print(type(content))
+parsed = RE_XLS_FILE.findall(content)
+parsed = filter(lambda x: x.endswith('.xls"'), parsed)
+# print(len(parsed))
+print(*parsed, sep='\n\n')
