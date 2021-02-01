5f4c087db319b3393ca4f18d27f7025cee6f7237
ь <das.lozh19@mail.ru>
2021-02-01 12:11:26 +0500

    request

--- 210201/step_2.py
+++ 210201/step_2.py
@@ -3,7 +3,8 @@ import re
 import requests
 
 # RE_XLS_FILE = re.compile(r'href=".+"')
-RE_XLS_FILE = re.compile(r'href="[^"]+"')
+# RE_XLS_FILE = re.compile(r'href="[^"]+\.xls"')
+RE_XLS_FILE = re.compile(r'href="([^"]+\.xls)"')
 
 request_url = 'https://kpk.kss45.ru/учебная-работа/расписание_пар.html'
 
@@ -11,6 +12,6 @@ response = requests.get(request_url)
 content = response.content.decode(encoding=response.encoding)
 
 parsed = RE_XLS_FILE.findall(content)
-parsed = filter(lambda x: x.endswith('.xls"'), parsed)
-# print(len(parsed))
+# parsed = filter(lambda x: x.endswith('.xls"'), parsed)
+print(len(parsed))
 print(*parsed, sep='\n\n')
---
+++ 210201/step_3.py
@@ -0,0 +1,18 @@
+import re
+
+import requests
+
+# RE_XLS_FILE = re.compile(r'href=".+"')
+# RE_XLS_FILE = re.compile(r'href="[^"]+\.xls"')
+RE_XLS_FILE = re.compile(r'href="([^"]+\.xls)"')
+
+request_url = 'https://kpk.kss45.ru/учебная-работа/расписание_пар.html'
+
+response = requests.get(request_url)
+content = response.content.decode(encoding=response.encoding)
+
+parsed = RE_XLS_FILE.findall(content)
+parsed = filter(lambda x: x.endswith('.xls'), parsed)
+print(type(parsed))
+print(len(list(parsed)))
+print(*parsed, sep='\n\n')
