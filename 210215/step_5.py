0880dc9ed08340361d6e2b9e99009850b1c44e3c
ь <das.lozh19@mail.ru>
2021-02-21 20:14:33 +0500

    decorator

--- 210215/step_5.py
+++ 210215/step_5.py
@@ -10,16 +10,28 @@ def str_to_int(func):
 
     return inner
 
+def str_to_float(func):
+    def inner(*args):
+        return func(*map(float, args))
 
-@str_to_int
-def my_sum(*nums):
-    return a + b
-print(sum(map(int, nums)))
+    return inner
 
+@str_to_float
+def nums_sum(*nums):
+    return sum( nums)
 
-a = input()
-b = input()
-print(my_sum(a, b))
+#
+# a = input()
+# b = input()
+# print(my_sum(a, b))
 # a  = 0
 # b = 17
 # print(random.randint(a, b))
+
+print(nums_sum(2, 3))
+print(nums_sum(2.3, 3.5))
+# a = input()
+# b = input()
+# print(my_sum(a, b))
+print(nums_sum('2', '3'))
+print(nums_sum('2.3', '3.5'))
