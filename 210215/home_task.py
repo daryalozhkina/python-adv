099bfa1d8202d2f421148bf54748dd90a35349b1
ь <das.lozh19@mail.ru>
2021-02-21 20:11:29 +0500

    decorator

--- 210215/home_task.py
+++ 210215/home_task.py
@@ -1,3 +1,15 @@
-def parse_str(src_str):
-    return src_str.str().split()
+def my_str(str_arg):
+    def the_decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return f'{str_arg}\n{result}\n{str_arg}'
+        return wrapper
+    return the_decorator
 
+
+@my_str('-')
+def decorated_function(input):
+    return input
+
+
+print(decorated_function('Сегодня понедельник'))
---
+++ 210215/step_6.py
@@ -0,0 +1,12 @@
+# @str_len
+def parse_str(src_str):
+    return src_str.strip().split()
+
+
+sample_1 = 'идет февраль месяц, скоро весна'
+sample_1_p = parse_str(sample_1)
+print(sample_1_p)
+
+sample_2 = 'идет февраль'
+sample_2_p = parse_str(sample_2)
+print(sample_2_p)
