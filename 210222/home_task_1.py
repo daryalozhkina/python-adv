c47bd2f681158867473437e8b6f05129a4174666
ь <das.lozh19@mail.ru>
2021-02-28 21:39:57 +0500

    hw

--- 210222/home_task_1.py
+++ 210222/home_task_1.py
@@ -4,17 +4,8 @@ from PIL import Image
 image_name = 'img.jpg'
 image = Image.open(image_name)
 image_np = np.array(image)
-# print(image_np.size, image_np.shape)
-# print(image_np) # uint8 -> 0..255
 image_np_conv = image_np
 image_np_conv = image_np  + 20
-# image_np_conv[:, :, 0] = 255
-# image_np_conv[:, :, 1] = 127
-# print(image_np[:, :, 0].mean(), image_np[:, :, 1].mean(),
-# image_np[:, :, 2].mean())
-# print(image_np_conv[:, :, 0].mean(), image_np_conv[:, :, 1].mean(),
-# image_np_conv[:, :, 2].mean())
-
 new_image = Image.fromarray(image_np_conv.astype('uint8'))
 save_name = 'img_conv.jpg'
 new_image.save(save_name)
