a19dbb50947af2143bd66daf276323e3606aa683
ь <das.lozh19@mail.ru>
2021-02-28 21:30:55 +0500

    hw

---
+++ 210222/step_1.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+dir_name = 'src'
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+else:
+    os.rmdir(dir_name)
+
+dir_name = 'docs/docker'
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+else:
+    shutil.rmtree(dir_name)
+
