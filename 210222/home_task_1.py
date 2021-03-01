e55864305a994e2adc34b5e58737f1bfd8aa36ce
ь <das.lozh19@mail.ru>
2021-03-01 09:34:12 +0500

    hw

--- 210222/home_task_1.py
+++ 210222/home_task_1.py
@@ -5,7 +5,7 @@ image_name = 'img.jpg'
 image = Image.open(image_name)
 image_np = np.array(image)
 image_np_conv = image_np
-image_np_conv += 10
+image_np_conv += 5
 new_image = Image.fromarray(image_np_conv.astype('uint8'))
 save_name = 'img_conv.jpg'
 new_image.save(save_name)
